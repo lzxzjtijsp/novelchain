ed7e84dd87f3eaa507436ac2f2ec40d72ba80cb9
lzxzjtijsp <shirok420@gmail.com>
2023-03-05 22:32:20 +0900

    Initial Comit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Box>
+        <h1>Hello World</h1>
+        <Heading as={'h1'}>Hello World</Heading>
+    </Box>
+  )
+}
